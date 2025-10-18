d08b68e930a4bfcf3692c8421c692334e166246a
iancorbett <iancorbett1324@gmail.com>
2025-10-17 23:21:50 -0700

    starting vehicle.php by defining classes, and now starting on a constructor function to build instances of the class

---
+++ classes/Vehicle.php
@@ -0,0 +1,11 @@
+<?php
+
+class Vehicle {
+    public string $make;
+    public string $model;
+    public int $year;
+    public string $package;
+    public float $price;
+
+    public function __construct 
+}
