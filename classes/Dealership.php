dcd691fa213c5bacebf483fc402705fa6078f600
iancorbett <iancorbett1324@gmail.com>
2025-10-18 09:30:19 -0700

    create function to return all inventory from a dealership

--- classes/Dealership.php
+++ classes/Dealership.php
@@ -4,7 +4,13 @@ require_once __DIR__ . '/Vehicle.php'; // run all code from Vehicle.php once
 Class Dealership {
     private array $inventory = []; //inventory of all vehicles
 
+    //void means function does not return anything
     public function addVehicle(Vehicle $vehicle): void { //Vehicle refers to type of variable that can be passed in (only a Vehicle object), $vehicle refers to the one that was actually passed in
         $this->inventory; //add this particular vehicle object to the dealership's inventory array
+    }
+
+    //this function will return an array
+    public function getInventory(): array {
+        return $this->inventory; //return all the inventory for this dealership
     }
 }
